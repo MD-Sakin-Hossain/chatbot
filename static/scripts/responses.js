230918109b604c7d781ab1d10eb6d9c3857b4893
MD-Sakin-Hossain <96043378+MD-Sakin-Hossain@users.noreply.github.com>
2021-12-23 12:50:09 +0600

    Update responses.js

--- static/scripts/responses.js
+++ static/scripts/responses.js
@@ -13,7 +13,49 @@ function getBotResponse(input) {
         return "Hello there!";
     } else if (input == "goodbye") {
         return "Talk to you later!";
-    } else {
+    }
+    
+    
+    
+    
+    else if (input == "Will you marry me?") {
+        return "I can't marry a Gay";
+    }
+    else if (input == "Will you marry me") {
+        return "I can't marry a Gay";
+    }
+    else if (input == "will you marry me") {
+        return "I can't marry a Gay";
+    }
+    else if (input == "will you marry me?") {
+        return "I can't marry a Gay";
+    }
+    
+    
+    
+    else if (input == "hey fucker") {
+        return "চুপ কর সালা মাদারচুদ";
+    }
+    else if (input == "fucker") {
+        return "চুপ কর সালা মাদারচুদ";
+    }else if (input == "fuck") {
+        return "চুপ কর সালা মাদারচুদ";
+    }else if (input == "Fuck") {
+        return "চুপ কর সালা মাদারচুদ";
+    }else if (input == "hey fucker") {
+        return "চুপ কর সালা মাদারচুদ";
+    }
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    else {
         return "Try asking something else!";
     }
 }
